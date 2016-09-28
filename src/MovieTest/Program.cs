6d787329e2ba2136893aeb44ef20b61da80b23c5
Adam Luchini <aluchini@gmail.com>
2016-09-28 14:24:11 -0700

    json request for year and title

--- src/MovieTest/Program.cs
+++ src/MovieTest/Program.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MovieTest
 {
@@ -9,6 +13,31 @@ namespace MovieTest
     {
         public static void Main(string[] args)
         {
+            var client = new RestClient("http://www.omdbapi.com/");
+            var request = new RestRequest("?t=argo&y=&plot=short&r.json", Method.GET);
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            {
+                JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+                Console.WriteLine(jsonResponse["Title"]);
+                Console.WriteLine(jsonResponse["Year"]);
+                Console.ReadLine();
+            }
+        }
+
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
         }
     }
 }
+
